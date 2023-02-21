d08796295d8feb0f133d55d361ffb9e81e45487f
Iris Sun <irissun@live.com>
2023-02-21 10:11:38 -0500

    Section 3 Step 1 - Implement findProductById() function

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -218,7 +218,11 @@ function loadMasterData() {
 }
 
 // Complete this function
-const findProductById = (id) => {};
+const findProductById = (id) => {
+    return function(product) {
+        return product.if === id;
+    }
+};
 
 // Complete this function
 const generateProductId = () => {};
