3952f4dee7204188c44ceb2a447b9a4c18089f8e
Iris Sun <irissun@live.com>
2023-02-21 17:17:29 -0500

    Section 4 Step 2 - Find Points for Expiry Date of the Product

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -229,11 +229,9 @@ const generateProductId = () => {
     return Math.ceil(Math.random() * 20);
 };
 
-
 const getProduct = (prodList, pId) => {
     return prodList.find(findProductById(pId));
 };
-
 
 // Complete this function
 const calculateBill = (prod, tBill) => {
@@ -258,10 +256,10 @@ const findPointsToBill = (roundedTotal) => {
     }
 };
 
-
 // Complete this function
-const findPointsForExpDate = (prod) => {};
-
+const findPointsForExpDate = (prod) => {
+    return prod.daysToExpire < 30 ? 10 : 0;
+};
 
 const calculatePoints = (prod, tBill) => {
     let pointsToBill = findPointsToBill(Math.round(tBill));
