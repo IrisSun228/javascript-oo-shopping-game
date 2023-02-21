80b858871f463fbd06c427a7b5667e0aa893e102
Iris Sun <irissun@live.com>
2023-02-21 09:35:41 -0500

    Section 2 Step 4 - Create and assign an instance of MagicProduct() to magProdObj !SECTION 2 COMPLETE!

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -137,7 +137,7 @@ const loadMagicProducts = (map, prodId) => {
                 const value = item[1];
 
                 // Create and assign an instance of MagicProduct to prodObj here
-                let magProdObj;
+                let magProdObj = new MagicProduct();
 
                 if (magProdObj != undefined && magProdObj != null) {
                     for (let i = 0; i < magProdKeys.length; i++) {
