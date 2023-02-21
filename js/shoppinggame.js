2a856cdf0bed53304a041566a82e028c019da967
Iris Sun <irissun@live.com>
2023-02-21 10:17:48 -0500

    Section 3 Step 2 - Generate and return a random integer from generatreProductId() function !SECTION 3 COMPLETE!

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -225,7 +225,9 @@ const findProductById = (id) => {
 };
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => {
+    return Math.ceil(Math.random() * 20);
+};
 
 
 const getProduct = (prodList, pId) => {
